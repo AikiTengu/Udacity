46e41746a00171d6b9e5acfe70c2a54ba25408c7
tengu <aiki_tengu@yahoo.com>
2020-11-11 17:03:20 +0200

    Tests run successfully. Added Ids to spans in chat.html in order to @FindBy

--- src/test/java/MyBatis/ChatPage.java
+++ src/test/java/MyBatis/ChatPage.java
@@ -1,4 +1,52 @@
 package MyBatis;
 
+import MyBatis.model.ChatMessage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Wait;
+
 public class ChatPage {
+
+
+    @FindBy(id = "messageText")
+    private WebElement textField;
+
+    @FindBy(id = "submitMessage")
+    private WebElement submitButton;
+
+    //@FindBys({@FindBy(className = "chatMessage), @FindBy (name = "username")}
+    @FindBy(id = "Username")
+    private WebElement firstMessageUsername;
+
+    @FindBy(id = "Message")
+    private WebElement firstMessageText;
+
+    @FindBy(id = "logoutButton")
+    private WebElement logoutButton;
+
+    public ChatPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+    public void sendChatMessage(String text) {
+        this.textField.sendKeys(text);
+        this.submitButton.click();
+    }
+
+    public ChatMessage getFirstMessage() {
+        ChatMessage result = new ChatMessage();
+       // result.setMessage("Hello Civ!");
+       // result.setUsername("Teddy");
+        result.setMessage(firstMessageText.getText());
+
+        result.setUsername(firstMessageUsername.getText());
+        return result;
+    }
+
+    public void pressLogoutButton() {
+        this.logoutButton.click();
+    }
 }
--- src/test/java/MyBatis/Lesson3ApplicationTests.java
+++ src/test/java/MyBatis/Lesson3ApplicationTests.java
@@ -1,13 +1,98 @@
 package MyBatis;
 
+import MyBatis.model.ChatMessage;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class Lesson3ApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@LocalServerPort
+	public int port;
+
+	public static WebDriver driver;
+
+	public String baseURL;
+
+	@BeforeAll
+	public static void beforeAll() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+
 	}
+
+	@AfterAll
+	public static void afterAll() {
+		driver.quit();
+		driver = null;
+	}
+
+	@BeforeEach
+	public void beforeEach() {
+		baseURL = baseURL = "http://localhost:" + port;
+	}
+
+
+	@Test
+	public void testUserSignup() {
+		String username = "Teddy";
+		String password = "qwerty";
+
+		driver.get(baseURL + "/signup");
+
+		SignupPage signupPage = new SignupPage(driver);
+		signupPage.signup("Theodore", "Rousevelt", username, password);
+	}
+
+	@Test
+	public void testUserLogin() {
+		String username = "Teddy";
+		String password = "qwerty";
+
+		driver.get(baseURL + "/login");
+
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.login(username, password);
+	}
+
+	@Test
+	public void testSubmitMessage() {
+		String username = "Teddy";
+		String password = "qwerty";
+		String userText = "Hello Civ!";
+
+
+		driver.get(baseURL + "/signup");
+
+		SignupPage signupPage = new SignupPage(driver);
+		signupPage.signup("Theodore", "Rousevelt", username, password);
+
+		driver.get(baseURL + "/login");
+
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.login(username, password);
+
+		ChatPage chatPage = new ChatPage(driver);
+
+		chatPage.sendChatMessage(userText);
+
+		ChatMessage sentMessage = chatPage.getFirstMessage();
+
+		assertEquals(username, sentMessage.getUsername());
+		assertEquals(userText, sentMessage.getMessage());
+
+		chatPage.pressLogoutButton();
+}
+
+
 
 }
